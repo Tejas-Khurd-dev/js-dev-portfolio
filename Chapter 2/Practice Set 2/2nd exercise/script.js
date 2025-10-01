aced7049525d648cdf9017beb8e0abf60a71b32d
Tejas-Khurd-dev <tejaskhurd.dev@gmail.com>
2025-10-01 23:44:08 +0530

    organize project structure, add new exercise and related files

---
+++ Chapter 2/Practice Set 2/2nd exercise/script.js
@@ -0,0 +1,9 @@
+let name = "Tejas";
+document.querySelector("#display-selector").innerText = name;
+
+const pi = 3.14;
+let radius = 5;
+let areaOfCircle = pi * radius * radius;
+console.log(`Area of circle is: ${areaOfCircle}`);
+
+console.log(eval("40+50"));
