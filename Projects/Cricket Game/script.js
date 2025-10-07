a98cbe2d8ed868283c4c62d8ba6bb530bb604607
Tejas-Khurd-dev <tejaskhurd.dev@gmail.com>
2025-10-07 20:27:51 +0530

    add parameter.html in chapter 4 and update cricket game index and script files in project directory

--- Projects/Cricket Game/script.js
+++ Projects/Cricket Game/script.js
@@ -1,1 +1,27 @@
-let randomNumber;
+function playGame(userChoice) {
+  const randomNumber = Math.floor(Math.random() * 3);
+  let computerChoice = "";
+
+  if (randomNumber === 0) computerChoice = "Bat";
+  else if (randomNumber === 1) computerChoice = "Ball";
+  else computerChoice = "Stump";
+
+  document.querySelector(
+    "#user-choice"
+  ).innerText = `You have chosen ${userChoice}`;
+  let result = "";
+
+  if (userChoice === computerChoice) {
+    result = `Computer chose ${computerChoice} → TIE!`;
+  } else if (
+    (userChoice === "Bat" && computerChoice === "Ball") ||
+    (userChoice === "Ball" && computerChoice === "Stump") ||
+    (userChoice === "Stump" && computerChoice === "Bat")
+  ) {
+    result = `Computer chose ${computerChoice} → YOU WIN!`;
+  } else {
+    result = `Computer chose ${computerChoice} → YOU LOSE!`;
+  }
+
+  document.querySelector("#result-declaration").innerText = result;
+}
