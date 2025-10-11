2ccdde9906fac83968dc4bc9610182db9b416c07
Tejas-Khurd-dev <tejaskhurd.dev@gmail.com>
2025-10-11 18:49:35 +0530

    add nested_object.html file in chapter 5 and update cricket game in projects

--- Projects/Cricket Game/script.js
+++ Projects/Cricket Game/script.js
@@ -1,3 +1,8 @@
+let score = {
+  win: 0,
+  loss: 0,
+  tie: 0,
+}
 function playGame(userChoice) {
   const randomNumber = Math.floor(Math.random() * 3);
   let computerChoice = "";
@@ -13,15 +18,21 @@ function playGame(userChoice) {
 
   if (userChoice === computerChoice) {
     result = `Computer chose ${computerChoice} → TIE!`;
+    score.tie ++;
   } else if (
     (userChoice === "Bat" && computerChoice === "Ball") ||
     (userChoice === "Ball" && computerChoice === "Stump") ||
     (userChoice === "Stump" && computerChoice === "Bat")
   ) {
     result = `Computer chose ${computerChoice} → YOU WIN!`;
+    score.win ++;
   } else {
     result = `Computer chose ${computerChoice} → YOU LOSE!`;
+    score.loss ++;
   }
 
   document.querySelector("#result-declaration").innerText = result;
+  document.querySelector("#win").innerText = score.win;
+  document.querySelector("#loss").innerText = score.loss;
+  document.querySelector("#tie").innerText = score.tie;
 }
