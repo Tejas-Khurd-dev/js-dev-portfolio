825442121b868f266d074aa1f598e9b850c11456
Tejas-Khurd-dev <tejaskhurd.dev@gmail.com>
2025-10-15 19:52:03 +0530

    added local storage in cricket game that store score data
    we can reset score data by clicking reset button

--- Projects/Cricket Game/script.js
+++ Projects/Cricket Game/script.js
@@ -1,11 +1,29 @@
-let score = {
-  win: 0,
-  loss: 0,
-  tie: 0,
-  displayScore() {
-    return `win = ${this.win}, loss = ${this.loss}, tie = ${this.tie}`;
-  },
+let scoreStr = localStorage.getItem("score");
+let score = JSON.parse(scoreStr) || {win: 0, loss: 0, tie: 0,};
+// let score;
+
+// if (scoreStr != undefined) {
+//   score = JSON.parse(scoreStr);
+// } else {
+//   score = {
+//     win: 0,
+//     loss: 0,
+//     tie: 0,
+//   };
+// }
+
+score.displayScore = function () {
+  return `Win = ${this.win}, Loss = ${this.loss}, Tie = ${this.tie}`;
 };
+
+function updateScoreUI() {
+  document.querySelector("#win").innerText = score.win;
+  document.querySelector("#loss").innerText = score.loss;
+  document.querySelector("#tie").innerText = score.tie;
+}
+
+updateScoreUI();
+
 function playGame(userChoice) {
   const randomNumber = Math.floor(Math.random() * 3);
   let computerChoice = "";
@@ -14,13 +32,12 @@ function playGame(userChoice) {
   else if (randomNumber === 1) computerChoice = "Ball";
   else computerChoice = "Stump";
 
-  document.querySelector(
-    "#user-choice"
-  ).innerText = `You have chosen ${userChoice}`;
+  document.querySelector("#user-choice").innerText = `You chose ${userChoice}`;
+
   let result = "";
 
   if (userChoice === computerChoice) {
-    result = `Computer chose ${computerChoice} → TIE!`;
+    result = `Computer chose ${computerChoice} → It's a TIE!`;
     score.tie++;
   } else if (
     (userChoice === "Bat" && computerChoice === "Ball") ||
@@ -30,14 +47,14 @@ function playGame(userChoice) {
     result = `Computer chose ${computerChoice} → YOU WIN!`;
     score.win++;
   } else {
-    result = `Computer chose ${computerChoice} → YOU LOSE!`;
+    result = `Computer chose ${computerChoice} → YOU LOSE`;
     score.loss++;
   }
 
-  // console.clear();
+  console.clear();
   console.log(score.displayScore());
   document.querySelector("#result-declaration").innerText = result;
-  document.querySelector("#win").innerText = score.win;
-  document.querySelector("#loss").innerText = score.loss;
-  document.querySelector("#tie").innerText = score.tie;
+
+  updateScoreUI();
+  localStorage.setItem("score", JSON.stringify(score));
 }
